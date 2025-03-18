583b0145134d7f18845c13f3dbbd0cf14e1cbfef
Silas <dev.silas.freitas@outlook.com>
2025-03-17 22:25:18 -0300

    feat(account): add delete use case

---
+++ src/domain/account/user/application/use-cases/delete-account-use-case.ts
@@ -0,0 +1,37 @@
+import { Either, left, right } from "@/core/either";
+import { Account } from "../../enterprise/entities/account";
+import { Injectable } from "@nestjs/common";
+import { AccountRepository } from "../repositories/account-repository";
+
+interface DeleteAccountUseCaseRequest {
+  accountId: string;
+}
+
+type DeleteAccountUseCaseResponse = Either<
+  null,
+  {
+    account: Account;
+  }
+>;
+
+@Injectable()
+export class DeleteAccountUseCase {
+
+  constructor (
+    private accountRepository: AccountRepository
+  ) {}
+
+  async execute (request: DeleteAccountUseCaseRequest): Promise<DeleteAccountUseCaseResponse> {
+    const account = await this.accountRepository.findById(request.accountId);
+
+    if (!account) {
+      return left(null);
+    }
+
+    await this.accountRepository.delete(account);
+
+    return right({
+      account,
+    });
+  }
+}
