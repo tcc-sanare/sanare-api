07593c7b74e77e4b941a48c99ecd9780b3b38437
Silas <dev.silas.freitas@outlook.com>
2025-03-17 22:21:08 -0300

    feat(account): add update use case

---
+++ src/domain/account/user/application/use-cases/update-my-account-use-case.ts
@@ -0,0 +1,43 @@
+import { Either, left, right } from "@/core/either";
+import { Account } from "../../enterprise/entities/account";
+import { Injectable } from "@nestjs/common";
+import { AccountRepository } from "../repositories/account-repository";
+
+interface UpdateMyAccountUseCaseRequest {
+  accountId: string;
+  name?: string;
+  theme?: "LIGHT" | "DARK";
+}
+
+type UpdateMyAccountUseCaseResponse = Either<
+  null,
+  {
+    account: Account;
+  }
+>;
+
+@Injectable()
+export class UpdateMyAccountUseCase {
+
+  constructor (
+    private accountRepository: AccountRepository,
+  ) {};
+
+  async execute (request: UpdateMyAccountUseCaseRequest): Promise<UpdateMyAccountUseCaseResponse> {
+    const account = await this.accountRepository.findById(request.accountId);
+
+    if (!account) {
+      return left(null);
+    }
+
+    account.name = request.name;
+    account.theme = request.theme;
+
+    await this.accountRepository.save(account);
+
+    return right({
+      account,
+    });
+  }
+
+};
