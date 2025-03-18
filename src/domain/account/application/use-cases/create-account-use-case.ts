898f6b3999a7ba217baf5269e33dc2ab39e4d25b
Silas <dev.silas.freitas@outlook.com>
2025-03-17 21:25:29 -0300

    feat(account): create account use case

---
+++ src/domain/account/application/use-cases/create-account-use-case.ts
@@ -0,0 +1,44 @@
+import { Either, left, right } from "@/core/either";
+import { Account } from "../../enterprise/entities/account";
+import { AccountRepository } from "../repositories/account-repository";
+import { Injectable } from "@nestjs/common";
+import { HashGenerator } from "../../cryptography/hash-generetor";
+
+interface CreateAccountUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+type CreateAccountUseCaseResponse = Either<
+  null,
+  {
+    account: Account;
+  }
+>;
+
+@Injectable()
+export class CreateAccountUseCase {
+  constructor (
+    private accountRepository: AccountRepository,
+    private hashGenerator: HashGenerator
+  ) {};
+
+  async execute (request: CreateAccountUseCaseRequest): Promise<CreateAccountUseCaseResponse> {
+    const account = await Account.create({
+      name: request.name,
+      email: request.email,
+      password: await this.hashGenerator.hash(request.password)
+    });
+
+    if (!account) {
+      return left(null);
+    }
+
+    await this.accountRepository.save(account);
+
+    return right({
+      account
+    });
+  }
+}
