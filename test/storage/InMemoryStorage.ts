0d890b756d69dd4b093a9b59d9b6b06760df1859
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-03-18 10:22:08 -0300

    test(*): inMemoryStorage to tests

---
+++ test/storage/InMemoryStorage.ts
@@ -0,0 +1,41 @@
+import { Storage } from "@/domain/application/storage/Storage";
+import { Blob, File } from "buffer";
+import { randomUUID } from "crypto";
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from "fs";
+
+interface StorageParams {
+  fileName: string;
+  fileType: string;
+  buffer: Buffer;
+}
+
+export class InMemoryStorage implements Storage {
+  async upload(params: StorageParams): Promise<{ fileKey: string; }> {
+    const fileKey = `${randomUUID()}-${params.fileName}`;
+
+    writeFileSync(`./test/storage/items/${fileKey}`, params.buffer);
+
+    return { fileKey };
+  }
+  async delete(fileKey: string): Promise<void> {
+    const path = `./test/storage/items/${fileKey}`;
+    if (!existsSync(path)) {
+      throw new Error("Image not found")
+    }
+
+    unlinkSync(path);
+  }
+  async getSignedUrl(fileKey: string): Promise<{ url: string; }> {
+    const path = `./test/storage/items/${fileKey}`;
+
+    if (!existsSync(path)) {
+      throw new Error("Image not found")
+    }
+    const fileBuffer = readFileSync(path);
+
+    const file = new Blob([fileBuffer]);
+
+    return { url: URL.createObjectURL(file) };
+  }
+  
+}
