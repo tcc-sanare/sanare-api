5d2158eb08287f5b28b8456dd4c5604c13e88d1c
Silas <dev.silas.freitas@outlook.com>
2025-03-20 19:14:39 -0300

    feat(allergy): add create and findAll methods

--- src/domain/medical/application/repositories/allergy-repository.ts
+++ src/domain/medical/application/repositories/allergy-repository.ts
@@ -1,6 +1,8 @@
 import { Allergy } from '../../enterprise/entities/allergy';
 
 export abstract class AllergyRepository {
+  abstract create(allergy: Allergy): Promise<void>;
+  abstract findAll(): Promise<Allergy[]>;
   abstract save(allergy: Allergy): Promise<void>;
   abstract delete(allergy: Allergy): Promise<void>;
   abstract findById(id: string): Promise<Allergy | null>;
