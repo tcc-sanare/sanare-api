1582ab70d67c6bb6d21d5183ecb5bbbade416196
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-03-20 08:39:43 -0300

    feat(allergy): create use case

---
+++ src/domain/medical/application/use-cases/create-allergy-use-case.ts
@@ -0,0 +1,42 @@
+import { Either, left, right } from "@/core/either";
+import { Allergy } from "../../enterprise/entities/allergy";
+import { All, Injectable } from "@nestjs/common";
+import { AllergyRepository } from "../repositories/allergy-repository";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+interface CreateAllergyUseCaseRequest {
+  name: string;
+  description?: string;
+}
+
+type CreateAllergyUseCaseResponse = Either<
+  null,
+  {
+    allergy: Allergy
+  }
+>;
+
+@Injectable()
+export class CreateAllergyUseCase {
+
+  constructor (
+    private allergyRepository: AllergyRepository
+  ) {};
+
+  async execute (data: CreateAllergyUseCaseRequest): Promise<CreateAllergyUseCaseResponse> {
+    const allergy = Allergy.create(
+      {
+        name: data.name,
+        description: data.description
+      }
+    );
+
+    try {
+      await this.allergyRepository.save(allergy);
+
+      return right({ allergy });
+    } catch {
+      return left(null);
+    }
+  }
+}
