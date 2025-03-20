e64262bcab9c96500efe9f2e205af5bbaa9ceccd
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-03-20 08:39:31 -0300

    feat(allergy): add entity

---
+++ src/domain/medical/enterprise/entities/allergy.ts
@@ -0,0 +1,51 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+export interface AllergyProps {
+  name: string;
+  description?: string;
+
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class Allergy extends Entity<AllergyProps> {
+
+  get name () {
+    return this.props.name;
+  }
+
+  set name (name: string) {
+    this.props.name = name;
+    this.update();
+  }
+
+  get description () {
+    return this.props.description;
+  }
+
+  set description (description: string) {
+    this.props.description = description;
+    this.update();
+  }
+
+  private update () {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(
+    props: Optional<AllergyProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const allergy = new Allergy(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id ?? new UniqueEntityID()
+    );
+
+    return allergy;
+  }
+}
