9ddd0c6d394265af2b140720520e3d86404d1778
Silas <dev.silas.freitas@outlook.com>
2025-03-22 15:03:11 -0300

    feat(chronic-disease): add delete use case

---
+++ src/domain/medical/application/use-cases/chronic-disease/delete-chronic-disease-use-case.ts
@@ -0,0 +1,34 @@
+import { Either, left, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
+import { ChronicDisease } from '../../../enterprise/entities/chronic-disease';
+import { ChronicDiseaseRepository } from '../../repositories/chronic-disease-repository';
+
+interface DeleteChronicDiseaseUseCaseRequest {
+  chronicDiseaseId: string;
+}
+
+type DeleteChronicDiseaseUseCaseResponse = Either<
+  null,
+  {
+    chronicDisease: ChronicDisease;
+  }
+>;
+
+@Injectable()
+export class DeleteChronicDiseaseUseCase {
+  constructor(private chronicDiseaseRepository: ChronicDiseaseRepository) {}
+
+  async execute(
+    request: DeleteChronicDiseaseUseCaseRequest,
+  ): Promise<DeleteChronicDiseaseUseCaseResponse> {
+    const chronicDisease = await this.chronicDiseaseRepository.findById(request.chronicDiseaseId);
+
+    if (!chronicDisease) {
+      return left(null);
+    }
+
+    await this.chronicDiseaseRepository.delete(chronicDisease);
+
+    return right({ chronicDisease });
+  }
+}
