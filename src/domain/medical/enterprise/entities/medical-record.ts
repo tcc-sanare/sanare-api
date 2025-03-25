a6628c810d9f8095dd679effac6e852904d69501
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-03-25 08:50:51 -0300

    feat(medical-record): add entity

---
+++ src/domain/medical/enterprise/entities/medical-record.ts
@@ -0,0 +1,42 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+export type BloodType = 'a-' | 'a+' | 'b-' | 'b+' | 'ab-' | 'ab+' | 'o-' | 'o+';
+
+export interface MedicalRecordProps {
+  bloodType: BloodType;
+
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class MedicalRecord extends Entity<MedicalRecordProps> {
+  get bloodType () {
+    return this.props.bloodType;
+  }
+
+  set bloodType (bloodType: BloodType) {
+    this.props.bloodType = bloodType;
+    this.update();
+  }
+
+  private update () {
+    this.props.updatedAt = new Date();
+  }
+
+  public static create (
+    props: Optional<MedicalRecordProps, 'createdAt'>,
+    id?: UniqueEntityID
+  ) {
+    const medicalRecord = new MedicalRecord(
+      {
+        ...props,
+        createdAt: new Date()
+      },
+      id ?? new UniqueEntityID()
+    );
+
+    return medicalRecord;
+  }
+}
