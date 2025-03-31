b3bc2b09b6db325a5c397947873c13d4cfe799d6
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-03-31 10:22:52 -0300

    feat(medical-record): add delete function to allergy relationship repository

--- src/domain/medical/application/repositories/medical-record-allergy-repository.ts
+++ src/domain/medical/application/repositories/medical-record-allergy-repository.ts
@@ -1,10 +1,11 @@
 import { MedicalRecordAllergy } from '../../enterprise/entities/medical-record-allergy';
 
 export abstract class MedicalRecordAllergiesRepository {
-  abstract createMany(MedicalRecordAllergy: MedicalRecordAllergy[]): Promise<void>;
-  abstract deleteMany(MedicalRecordAllergy: MedicalRecordAllergy[]): Promise<void>;
+  abstract createMany(medicalRecordAllergies: MedicalRecordAllergy[]): Promise<void>;
+  abstract deleteMany(medicalRecordAllergies: MedicalRecordAllergy[]): Promise<void>;
   abstract findManyByMedicalRecordId(
     medicalRecordId: string,
   ): Promise<MedicalRecordAllergy[] | null>;
+  abstract deleteManyByMedicalRecordId(medicalRecordId: string): Promise<void>;
   abstract deleteManyByAllergyId(allergyId: string): Promise<void>;
 }
