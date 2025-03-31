10f59ee80494e009b131bdf03d044a423553b346
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-03-31 09:27:10 -0300

    fix(medical-record): add allergies and chronic diseases relationship

--- src/domain/medical/application/use-cases/medical-record/create-medical-record-use-case.ts
+++ src/domain/medical/application/use-cases/medical-record/create-medical-record-use-case.ts
@@ -2,6 +2,13 @@ import { Either, left, right } from "@/core/either";
 import { BloodType, MedicalRecord } from "@/domain/medical/enterprise/entities/medical-record";
 import { MedicalRecordRepository } from "../../repositories/medical-record-repository";
 import { Injectable } from "@nestjs/common";
+import { AllergyRepository } from "../../repositories/allergy-repository";
+import { MedicalRecordAllergiesRepository } from "../../repositories/medical-record-allergy-repository";
+import { MedicalRecordChronicDiseasesRepository } from "../../repositories/medical-record-chronic-disease";
+import { MedicalRecordAllergy } from "@/domain/medical/enterprise/entities/medical-record-allergy";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { ChronicDisease } from "@/domain/medical/enterprise/entities/chronic-disease";
+import { MedicalRecordChronicDisease } from "@/domain/medical/enterprise/entities/medical-record-chronic-disease";
 
 interface CreateMedicalRecordUseCaseRequest {
   bloodType: BloodType;
@@ -22,6 +29,8 @@ type CreateMedicalRecordUseCaseResponse = Either<
 export class CreateMedicalRecordUseCase {
   constructor(
     private medicalRecordRepository: MedicalRecordRepository,
+    private medicalRecordAllergiesRepository: MedicalRecordAllergiesRepository,
+    private medicalRecordChronicDiseasesRepository: MedicalRecordChronicDiseasesRepository
   ) {}
 
   async execute (
@@ -30,11 +39,26 @@ export class CreateMedicalRecordUseCase {
     const medicalRecord = MedicalRecord.create({
       bloodType: data.bloodType,
       userId: data.userId
-
     });
 
     try {
       await this.medicalRecordRepository.create(medicalRecord);
+
+      const medicalRecordAllergies = data.allergies
+        .map(allergy => MedicalRecordAllergy.create({
+          medicalRecordId: medicalRecord.id,
+          allergyId: new UniqueEntityID(allergy)
+        }));
+
+      await this.medicalRecordAllergiesRepository.createMany(medicalRecordAllergies);
+
+      const medicalRecordChronicDiseases = data.chronicDiseases
+        .map(chronicDisease => MedicalRecordChronicDisease.create({
+          medicalRecordId: medicalRecord.id,
+          chronicDiseaseId: new UniqueEntityID(chronicDisease)
+        }));
+
+      await this.medicalRecordChronicDiseasesRepository.createMany(medicalRecordChronicDiseases);
 
       return right({medicalRecord});
     } catch {
