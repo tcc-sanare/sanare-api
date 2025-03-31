67021047f41d2c45104764aad56c1fd393793aaf
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-03-31 10:23:30 -0300

    test(medical-record): add allergy relationship in memory repository

---
+++ test/repositories/in-memory-medical-record-allergy-repository.ts
@@ -0,0 +1,41 @@
+import { MedicalRecordAllergiesRepository } from "@/domain/medical/application/repositories/medical-record-allergy-repository";
+import { MedicalRecordAllergy } from "@/domain/medical/enterprise/entities/medical-record-allergy";
+
+export class InMemoryMedicalRecordAllergiesRepository implements MedicalRecordAllergiesRepository {
+
+  items: MedicalRecordAllergy[]
+
+  constructor () {
+    this.items = [];
+  }
+
+  async createMany(medicalRecordAllergies: MedicalRecordAllergy[]): Promise<void> {
+    this.items.push(...medicalRecordAllergies);
+  }
+
+  async deleteMany(medicalRecordAllergies: MedicalRecordAllergy[]): Promise<void> {
+    medicalRecordAllergies.forEach(medicalRecordAllergy => {
+      const medicalRecordAllergyIndex = this.items
+        .findIndex(item => item.allergyId.toString() === medicalRecordAllergy.allergyId.toString() && item.medicalRecordId.toString() === medicalRecordAllergy.medicalRecordId.toString());
+
+      if (medicalRecordAllergyIndex === -1) {
+        return;
+      }
+
+      this.items.splice(medicalRecordAllergyIndex, 1);
+    });
+  }
+
+  async findManyByMedicalRecordId(medicalRecordId: string): Promise<MedicalRecordAllergy[]> {
+    return this.items.filter(item => item.medicalRecordId.toString() === medicalRecordId);
+  }
+
+  async deleteManyByMedicalRecordId(medicalRecordId: string): Promise<void> {
+    this.items = this.items.filter(item => item.medicalRecordId.toString() !== medicalRecordId);
+  }
+
+  async deleteManyByAllergyId(allergyId: string): Promise<void> {
+    this.items = this.items.filter(item => item.allergyId.toString() !== allergyId);
+  }
+  
+}
