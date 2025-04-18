f5d1b2e4a4e830d50af4a17680f115ccc15a6e09
Silas <dev.silas.freitas@outlook.com>
2025-04-18 09:49:35 -0300

    feat(medical-record): update use case

---
+++ src/domain/medical/application/use-cases/medical-record/update-medical-record-use-case.ts
@@ -0,0 +1,71 @@
+import { Either, left, right } from '@/core/either';
+import { Injectable } from '@nestjs/common';
+import { MedicalRecordRepository } from '../../repositories/medical-record-repository';
+import {
+  BloodType,
+  MedicalRecord,
+} from '@/domain/medical/enterprise/entities/medical-record';
+import { MedicalRecordAllergy } from '@/domain/medical/enterprise/entities/medical-record-allergy';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { MedicalRecordChronicDisease } from '@/domain/medical/enterprise/entities/medical-record-chronic-disease';
+
+interface UpdateMedicalRecordUseCaseRequest {
+  medicalRecordId: string;
+  bloodType?: BloodType;
+  allergies?: string[];
+  chronicDiseases?: string[];
+}
+
+type UpdateMedicalRecordUseCaseResponse = Either<
+  null,
+  {
+    medicalRecord: MedicalRecord;
+  }
+>;
+
+@Injectable()
+export class UpdateMedicalRecordUseCase {
+  constructor(private medicalRecordRepository: MedicalRecordRepository) {}
+
+  async execute(
+    data: UpdateMedicalRecordUseCaseRequest,
+  ): Promise<UpdateMedicalRecordUseCaseResponse> {
+    const medicalRecord = await this.medicalRecordRepository.findById(
+      data.medicalRecordId,
+    );
+
+    if (!medicalRecord) {
+      return left(null);
+    }
+
+    if (data.bloodType) {
+      medicalRecord.bloodType = data.bloodType;
+    }
+
+    if (data.allergies) {
+      medicalRecord.allergies.update(
+        data.allergies.map((allergy) =>
+          MedicalRecordAllergy.create({
+            medicalRecordId: medicalRecord.id,
+            allergyId: new UniqueEntityID(allergy),
+          }),
+        ),
+      );
+    }
+
+    if (data.chronicDiseases) {
+      medicalRecord.chronicDiseases.update(
+        data.chronicDiseases.map((chronicDisease) =>
+          MedicalRecordChronicDisease.create({
+            medicalRecordId: medicalRecord.id,
+            chronicDiseaseId: new UniqueEntityID(chronicDisease),
+          }),
+        ),
+      );
+    }
+
+    await this.medicalRecordRepository.save(medicalRecord);
+
+    return right({ medicalRecord });
+  }
+}
