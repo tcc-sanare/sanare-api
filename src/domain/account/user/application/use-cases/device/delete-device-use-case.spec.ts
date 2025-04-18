b5a4a0f3b1635544e2a446f1f197209d9e19e3c0
Silas <dev.silas.freitas@outlook.com>
2025-04-18 11:01:14 -0300

    test(device): add delete use case

---
+++ src/domain/account/user/application/use-cases/device/delete-device-use-case.spec.ts
@@ -0,0 +1,55 @@
+import { InMemoryDeviceRepository } from "test/repositories/in-memory-device-repository";
+import { DeleteDeviceUseCase } from "./delete-device-use-case";
+import { makeDevice } from "test/factories/make-device";
+
+describe("DeleteDeviceUseCase", () => {
+  let sut: DeleteDeviceUseCase;
+  let inMemoryDeviceRepository: InMemoryDeviceRepository;
+
+  beforeEach(() => {
+    inMemoryDeviceRepository = new InMemoryDeviceRepository();
+    sut = new DeleteDeviceUseCase(inMemoryDeviceRepository);
+  });
+
+  it("should be able to delete a device", async () => {
+    const device = makeDevice();
+    await inMemoryDeviceRepository.create(device);
+
+    const result = await sut.execute({
+      token: device.token,
+      userId: device.userId.toString(),
+    });
+
+    expect(result.isRight()).toBeTruthy();
+    expect(result.value).toBeUndefined();
+    expect(inMemoryDeviceRepository.items).toHaveLength(0);
+  });
+
+  it("should not be able to delete a device with a different userId", async () => {
+    const device = makeDevice();
+    await inMemoryDeviceRepository.create(device);
+
+    const result = await sut.execute({
+      token: device.token,
+      userId: "different-user-id",
+    });
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toBeNull();
+    expect(inMemoryDeviceRepository.items).toHaveLength(1);
+  });
+
+  it("should not be able to delete a non-existing device", async () => {
+    const device = makeDevice();
+    await inMemoryDeviceRepository.create(device);
+
+    const result = await sut.execute({
+      token: "non-existing-token",
+      userId: device.userId.toString(),
+    });
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toBeNull();
+    expect(inMemoryDeviceRepository.items).toHaveLength(1);
+  });
+});
