f9d39f9be23899d440ac89aaf9c3975fd5cdb7c0
Silas <dev.silas.freitas@outlook.com>
2025-04-18 09:47:36 -0300

    test(medical-record): get by id use case

---
+++ src/domain/medical/application/use-cases/medical-record/get-medical-record-by-id-use-case.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryMedicalRecordRepository } from 'test/repositories/in-memory-medical-record-repository';
+import { GetMedicalRecordByIdUseCase } from './get-medical-record-by-id-use-case';
+import { makeMedicalRecord } from 'test/factories/make-medical-record';
+import { MedicalRecord } from '@/domain/medical/enterprise/entities/medical-record';
+
+describe('GetMedicalRecordUseCase', () => {
+  let sut: GetMedicalRecordByIdUseCase;
+  let inMemoryMedicalRecordRepository: InMemoryMedicalRecordRepository;
+
+  beforeEach(() => {
+    inMemoryMedicalRecordRepository = new InMemoryMedicalRecordRepository();
+    sut = new GetMedicalRecordByIdUseCase(inMemoryMedicalRecordRepository);
+  });
+
+  it('should get a medical record by id', async () => {
+    const medicalRecord = makeMedicalRecord();
+    inMemoryMedicalRecordRepository.items.push(medicalRecord);
+
+    const result = await sut.execute({
+      medicalRecordId: medicalRecord.id.toString(),
+    });
+
+    expect(result.isRight()).toBeTruthy();
+    expect(result.value.medicalRecord).toEqual(medicalRecord);
+    expect(result.value.medicalRecord).toBeInstanceOf(MedicalRecord);
+  });
+
+  it('should not get a medical record if it does not exist', async () => {
+    const medicalRecord = makeMedicalRecord();
+    inMemoryMedicalRecordRepository.items.push(medicalRecord);
+
+    const result = await sut.execute({
+      medicalRecordId: 'non-existing-id',
+    });
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toBeNull();
+  });
+});
