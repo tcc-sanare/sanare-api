cb4abbbf9b41a92614a38f820326624ed491983b
Silas <dev.silas.freitas@outlook.com>
2025-04-22 20:05:53 -0300

    test(self-monitor): add update use case

---
+++ src/domain/medical/application/use-cases/self-monitor/update-self-monitor-use-case.spec.ts
@@ -0,0 +1,40 @@
+import { SelfMonitor } from "@/domain/medical/enterprise/entities/self-monitor";
+import { makeSelfMonitor } from "test/factories/make-self-monitor";
+import { InMemorySelfMonitorRepository } from "test/repositories/in-memory-self-monitor-repository";
+import { UpdateSelfMonitorUseCase } from "./update-self-monitor-use-case";
+
+describe('UpdateSelfMonitorUseCase', () => {
+  let sut: UpdateSelfMonitorUseCase;
+  let inMemorySelfMonitorRepository: InMemorySelfMonitorRepository;
+
+  beforeEach(() => {
+    inMemorySelfMonitorRepository = new InMemorySelfMonitorRepository();
+    sut = new UpdateSelfMonitorUseCase(inMemorySelfMonitorRepository);
+  });
+
+  it('should be able to update a self monitor', async () => {
+    const selfMonitor = makeSelfMonitor();
+
+    inMemorySelfMonitorRepository.items.push(selfMonitor);
+
+    const result = await sut.execute({
+      selfMonitorId: selfMonitor.id.toString(),
+      caregiverId: 'new-caregiver-id',
+    });
+
+    expect(result.isRight()).toBeTruthy();
+    expect(result.value.selfMonitor).toBeInstanceOf(SelfMonitor);
+    expect(result.value.selfMonitor.id.toString()).toEqual(selfMonitor.id.toString());
+    expect(result.value.selfMonitor.caregiverId.toString()).toEqual('new-caregiver-id');
+  });
+
+  it('should not be able to update a self monitor if it does not exist', async () => {
+    const result = await sut.execute({
+      selfMonitorId: 'non-existing-id',
+      caregiverId: 'new-caregiver-id',
+    });
+
+    expect(result.isLeft()).toBeTruthy();
+    expect(result.value).toBeNull();
+  });
+});
