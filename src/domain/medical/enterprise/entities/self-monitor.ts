99daeda36ee0790a43b0298b5f38a7724364e7e8
Silas <dev.silas.freitas@outlook.com>
2025-04-22 19:59:44 -0300

    feat(self-monitor): add entity

---
+++ src/domain/medical/enterprise/entities/self-monitor.ts
@@ -0,0 +1,53 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+
+export interface SelfMonitorProps {
+  userId: UniqueEntityID;
+  caregiverId?: UniqueEntityID;
+
+  createdAt: Date;
+  updatedAt?: Date;
+};
+
+export class SelfMonitor extends Entity<SelfMonitorProps> {
+  get userId() {
+    return this.props.userId;
+  }
+
+  get caregiverId() {
+    return this.props.caregiverId;
+  }
+
+  set caregiverId(caregiverId: UniqueEntityID) {
+    this.props.caregiverId = caregiverId;
+    this.update();
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private update() {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(
+    props: Optional<SelfMonitorProps, "createdAt">,
+    id?: UniqueEntityID
+  ) {
+    const selfMonitor = new SelfMonitor(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id ?? new UniqueEntityID()
+    );
+
+    return selfMonitor;
+  }
+}
