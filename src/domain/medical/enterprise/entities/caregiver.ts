5e08b5f578ca75cfd427b0638ae3a1a373ac6956
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-04-25 10:08:12 -0300

    feat(caregiver): add entity

---
+++ src/domain/medical/enterprise/entities/caregiver.ts
@@ -0,0 +1,50 @@
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Optional } from '@/core/types/optional';
+import { UniqueCaregiverCode } from './value-object/unique-caregiver-code';
+
+export interface CaregiverProps {
+  code: UniqueCaregiverCode;
+  userId: UniqueEntityID;
+
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class Caregiver extends Entity<CaregiverProps> {
+  get code() {
+    return this.props.code;
+  }
+
+  get userId() {
+    return this.props.userId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  private update() {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(
+    props: Optional<Caregiver, 'createdAt' | 'code'>,
+    id?: UniqueEntityID,
+  ) {
+    const caregiver = new Caregiver(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+        code: props.code ?? new UniqueCaregiverCode(),
+      },
+      id ?? new UniqueEntityID(),
+    );
+
+    return caregiver;
+  }
+}
