2968e185f6e41f5af36896b01e58c13166ef4a43
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-04-29 10:39:45 -0300

    feat: use case error class

--- src/core/errors/use-case-error.ts
+++ src/core/errors/use-case-error.ts
@@ -1,3 +1,13 @@
-export interface UseCaseError {
-  message: string;
+export interface UseCaseErrorProps<T> {
+  statusCode: number;
+  errors: {
+    path?: T[];
+    message: string;
+  }[]
+}
+
+export class UseCaseError<T> extends Error {
+  constructor (props: UseCaseErrorProps<keyof T>) {
+    super(JSON.stringify(props));
+  }
 }
