7e63b3fa96c8f0be774c71b8d68fbeabba76ea61
SILAS SILVA <dev.silas.freitas@outlook.com>
2025-04-29 09:35:18 -0300

    fix(email-verification): change from 6 chars to 4 chars in email verification code

--- src/domain/account/user/enterprise/entities/value-object/unique-email-verification-code.ts
+++ src/domain/account/user/enterprise/entities/value-object/unique-email-verification-code.ts
@@ -2,6 +2,6 @@ import { UniqueCode } from "@/core/entities/unique-code";
 
 export class UniqueEmailVerificationCode extends UniqueCode {
   constructor(value?: string) {
-    super({ length: 6, chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' }, value);
+    super({ length: 4, chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890' }, value);
   }
 }
