a314ba336df8b9e43d1f1785b3a2f6144f52ff21
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:30:19 -0300

    feat(caregiver): add prisma repository

---
+++ src/infra/database/prisma/repositories/medical/prisma-caregiver-repository.ts
@@ -0,0 +1,82 @@
+import { CaregiverRepository } from "@/domain/medical/application/repositories/caregiver-repository";
+import { PrismaCaregiverMapper } from "../../mappers/medical/prisma-caregiver-mapper";
+import { Caregiver } from "@/domain/medical/enterprise/entities/caregiver";
+import { PrismaService } from "../../prisma.service";
+import { Injectable } from "@nestjs/common";
+import { UniqueCaregiverCode } from "@/domain/medical/enterprise/entities/value-object/unique-caregiver-code";
+
+@Injectable()
+export class PrismaCaregiverRepository implements CaregiverRepository {
+  constructor (
+    private prisma: PrismaService
+  ) {}
+
+  async findByCaregiverCode(caregiverCode: UniqueCaregiverCode): Promise<Caregiver | null> {
+    const caregiver = await this.prisma.caregiver.findFirst({
+      where: {
+        code: caregiverCode.toValue(),
+      },
+    });
+
+    if (!caregiver) {
+      return null;
+    }
+
+    return PrismaCaregiverMapper.toDomain(caregiver);
+  }
+  
+  async findById(id: string): Promise<Caregiver | null> {
+    const caregiver = await this.prisma.caregiver.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!caregiver) {
+      return null;
+    }
+
+    return PrismaCaregiverMapper.toDomain(caregiver);
+  }
+  
+  async findByUserId(userId: string): Promise<Caregiver | null> {
+    const caregiver = await this.prisma.caregiver.findFirst({
+      where: {
+        userId,
+      },
+    });
+
+    if (!caregiver) {
+      return null;
+    }
+
+    return PrismaCaregiverMapper.toDomain(caregiver);
+  }
+
+  async create(caregiver: Caregiver): Promise<void> {
+    const data = PrismaCaregiverMapper.toPrisma(caregiver);
+
+    await this.prisma.caregiver.create({
+      data,
+    });
+  }
+
+  async save(caregiver: Caregiver): Promise<void> {
+    const data = PrismaCaregiverMapper.toPrisma(caregiver);
+
+    await this.prisma.caregiver.update({
+      where: {
+        id: caregiver.id.toString(),
+      },
+      data,
+    });
+  }
+
+  async delete(caregiver: Caregiver): Promise<void> {
+    await this.prisma.caregiver.delete({
+      where: {
+        id: caregiver.id.toString(),
+      },
+    });
+  }
+}
