d9072e13d84c46f7acc8a15137e4586feaa31f52
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:36:17 -0300

    feat(allergy): add get by id controller

---
+++ src/infra/http/controllers/medical/allergy/get-allergy-by-id-controller.ts
@@ -0,0 +1,21 @@
+import { GetAllergyByIdUseCase } from "@/domain/medical/application/use-cases/allergy/get-allergy-by-id-use-case";
+import { AllergyPresenter } from "@/infra/http/presenters/allergy-presenter";
+import { Controller, Get, Param } from "@nestjs/common";
+
+@Controller('allergies/:allergyId')
+export class GetAllergyByIdController {
+  constructor (
+    private getAllergyByIdUseCase: GetAllergyByIdUseCase,
+  ) {}
+
+  @Get()
+  async handle(@Param('allergyId') allergyId: string) {
+    const result = await this.getAllergyByIdUseCase.execute({ allergyId });
+
+    if (result.isLeft()) {
+      return result.value.message;
+    }
+
+    return AllergyPresenter.toHttp(result.value.allergy);
+  }
+}
