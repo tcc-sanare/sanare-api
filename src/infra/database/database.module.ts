7d6ed2f2e38b5abea911acf5ca8169f394168786
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:32:22 -0300

    feat(db): add prisma repositories on database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { AllergyRepository } from '@/domain/medical/application/repositories/allergy-repository';
+import { PrismaAllergyRepository } from './prisma/repositories/medical/prisma-allergy-repository';
 
 @Module({
-  providers: [],
+  providers: [
+    PrismaService,
+    {
+      provide: AllergyRepository,
+      useClass: PrismaAllergyRepository
+    }
+  ],
   controllers: [],
   imports: [],
-  exports: [],
+  exports: [
+    PrismaService,
+    AllergyRepository
+  ],
 })
-export class HttpModule {}
+export class DatabaseModule {}
