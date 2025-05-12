4edd7dba7b29c531e05e6df39e7f887d187b94f5
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:37:11 -0300

    feat(*): add http and database module to app module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { HttpModule } from './http/http.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    HttpModule
+  ],
   controllers: [],
   providers: [],
 })
