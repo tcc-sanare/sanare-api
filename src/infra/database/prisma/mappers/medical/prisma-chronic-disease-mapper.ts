55790fa791a3871754fd4b847da841d02ae56b71
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:27:59 -0300

    feat(chronic-disease): add prisma mapper

---
+++ src/infra/database/prisma/mappers/medical/prisma-chronic-disease-mapper.ts
@@ -0,0 +1,26 @@
+import { Prisma, ChronicDiseases as PrismaChronicDisease } from "@prisma/client";
+import { ChronicDisease } from "@/domain/medical/enterprise/entities/chronic-disease";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+export class PrismaChronicDiseaseMapper {
+  static toDomain(raw: PrismaChronicDisease): ChronicDisease {
+    return ChronicDisease.create({
+      name: raw.name,
+      description: raw.description,
+      iconKey: raw.iconKey,
+      createdAt: raw.createdAt,
+      updatedAt: raw.updatedAt,
+    }, new UniqueEntityID(raw.id));
+  }
+
+  static toPrisma(chronicDisease: ChronicDisease): Prisma.ChronicDiseasesUncheckedCreateInput {
+    return {
+      id: chronicDisease.id.toString(),
+      name: chronicDisease.name,
+      description: chronicDisease.description,
+      iconKey: chronicDisease.iconKey,
+      createdAt: chronicDisease.createdAt,
+      updatedAt: chronicDisease.updatedAt,
+    };
+  }
+}
