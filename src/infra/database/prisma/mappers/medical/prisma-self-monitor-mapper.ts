57af030701bc530e0bf137a19fdf646a6121948b
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:28:30 -0300

    feat(self-monitor): add prisma mapper

---
+++ src/infra/database/prisma/mappers/medical/prisma-self-monitor-mapper.ts
@@ -0,0 +1,26 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { SelfMonitor, SelfMonitorLogInput } from "@/domain/medical/enterprise/entities/self-monitor";
+import { SelfMonitor as PrismaSelfMonitor, Prisma } from "@prisma/client";
+
+export class PrismaSelfMonitorMapper {
+  static toDomain(raw: PrismaSelfMonitor): SelfMonitor {
+    return SelfMonitor.create({
+      logInputs: raw.logInputs as unknown as SelfMonitorLogInput,
+      userId: new UniqueEntityID(raw.userId),
+      caregiverId: raw.caregiverId ? new UniqueEntityID(raw.caregiverId) : undefined,
+      createdAt: raw.createdAt,
+      updatedAt: raw.updatedAt,
+    }, new UniqueEntityID(raw.id));
+  }
+
+  static toPrisma(selfMonitor: SelfMonitor): Prisma.SelfMonitorUncheckedCreateInput {
+    return {
+      id: selfMonitor.id.toString(),
+      userId: selfMonitor.userId.toString(),
+      caregiverId: selfMonitor.caregiverId?.toString(),
+      logInputs: selfMonitor.logInputs,
+      createdAt: selfMonitor.createdAt,
+      updatedAt: selfMonitor.updatedAt,
+    };
+  }
+}
