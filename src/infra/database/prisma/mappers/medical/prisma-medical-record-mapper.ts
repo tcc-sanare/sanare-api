13da85c42ec25f837dd81b8c9f72e954f0d68a71
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:28:10 -0300

    feat(medical-record): add prisma mapper

---
+++ src/infra/database/prisma/mappers/medical/prisma-medical-record-mapper.ts
@@ -0,0 +1,62 @@
+import { 
+  Prisma, 
+  MedicalRecord as PrismaMedicalRecord, 
+  BloodType as PrismaBloodType, 
+  MedicalRecordToAllergies as PrismaMedicalRecordToAllergies, 
+  MedicalRecordToChronicDiseases as PrismaMedicalRecordToChronicDisease,
+} from "@prisma/client";
+import { BloodType, MedicalRecord } from "@/domain/medical/enterprise/entities/medical-record";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { MedicalRecordAllergyList } from "@/domain/medical/enterprise/entities/medical-record-allergy-list";
+import { MedicalRecordAllergy } from "@/domain/medical/enterprise/entities/medical-record-allergy";
+import { MedicalRecordChronicDiseaseList } from "@/domain/medical/enterprise/entities/medical-record-chronic-disease-list";
+import { MedicalRecordChronicDisease } from "@/domain/medical/enterprise/entities/medical-record-chronic-disease";
+
+function bloodTypeToDomain(bloodType: PrismaBloodType): BloodType {
+  switch (bloodType) {
+    case PrismaBloodType.A_MINUS:
+      return 'a-';
+    case PrismaBloodType.A_PLUS:
+      return 'a+';
+    case PrismaBloodType.B_MINUS:
+      return 'b-';
+    case PrismaBloodType.B_PLUS:
+      return 'b+';
+    case PrismaBloodType.AB_MINUS:
+      return 'ab-';
+    case PrismaBloodType.AB_PLUS:
+      return 'ab+';
+    case PrismaBloodType.O_MINUS:
+      return 'o-';
+    case PrismaBloodType.O_PLUS:
+      return 'o+';
+  }
+}
+
+export class PrismaMedicalRecordMapper {
+  static toDomain(raw: PrismaMedicalRecord & {
+    allergies: PrismaMedicalRecordToAllergies[];
+    chronicDiseases: PrismaMedicalRecordToChronicDisease[];
+  }): MedicalRecord {
+    return MedicalRecord.create({
+      selfMonitorId: raw.selfMonitorId ? new UniqueEntityID(raw.selfMonitorId) : undefined,
+      bloodType: bloodTypeToDomain(raw.bloodType),
+      allergies: new MedicalRecordAllergyList(raw.allergies.map(medicalRecordToAllergy => MedicalRecordAllergy.create({
+        allergyId: new UniqueEntityID(medicalRecordToAllergy.medicalRecordId),
+        medicalRecordId: new UniqueEntityID(medicalRecordToAllergy.medicalRecordId),
+      }))),
+      chronicDiseases: new MedicalRecordChronicDiseaseList(raw.chronicDiseases.map(medicalRecordToChronicDisease => MedicalRecordChronicDisease.create({
+        chronicDiseaseId: new UniqueEntityID(medicalRecordToChronicDisease.medicalRecordId),
+        medicalRecordId: new UniqueEntityID(medicalRecordToChronicDisease.medicalRecordId),
+      }))),
+    }, new UniqueEntityID(raw.id));
+  }
+
+  static toPrisma(medicalRecord: MedicalRecord): Prisma.MedicalRecordUncheckedCreateInput {
+    return {
+      id: medicalRecord.id.toString(),
+      selfMonitorId: medicalRecord.selfMonitorId?.toString(),
+      bloodType: PrismaBloodType[medicalRecord.bloodType],
+    };
+  }
+}
