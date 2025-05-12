6cdedbc47e69f0052332beb0a8cd9bde8f485e0c
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:36:46 -0300

    feat(http): add controllers and use cases to http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,33 @@
 import { Module } from '@nestjs/common';
+import { GetAllAllergiesController } from './controllers/medical/allergy/get-all-allergies-controller';
+import { DatabaseModule } from '../database/database.module';
+import { GetAllAllergiesUseCase } from '@/domain/medical/application/use-cases/allergy/get-all-allergies-use-case';
+import { GetAllergyByIdController } from './controllers/medical/allergy/get-allergy-by-id-controller';
+import { GetAllergyByIdUseCase } from '@/domain/medical/application/use-cases/allergy/get-allergy-by-id-use-case';
+import { GetAllergiesByNameUseCase } from '@/domain/medical/application/use-cases/allergy/get-allergies-by-name-use-case';
+import { CreateAllergyController } from './controllers/medical/allergy/create-allergy-controller';
+import { CreateAllergyUseCase } from '@/domain/medical/application/use-cases/allergy/create-allergy-use-case';
+import { InMemoryStorage } from 'test/storage/in-memory-storage';
+import { Storage } from '@/domain/application/storage';
 
 @Module({
-  providers: [],
-  controllers: [],
-  imports: [],
+  controllers: [
+    GetAllAllergiesController,
+    GetAllergyByIdController,
+    CreateAllergyController
+  ],
+  providers: [
+    GetAllAllergiesUseCase,
+    GetAllergiesByNameUseCase,
+    GetAllergyByIdUseCase,
+    CreateAllergyUseCase,
+    {
+      provide: Storage,
+      useClass: InMemoryStorage
+    }
+  ],
+  imports: [
+    DatabaseModule
+  ],
 })
 export class HttpModule {}
