211aa2cdd8adb88829ca258f173d51e95f4eb5a7
Silas <dev.silas.freitas@outlook.com>
2025-05-11 22:26:29 -0300

    feat(*): config swagger in main

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sanare API')
+    .setDescription('Sanare API documentation')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    jsonDocumentUrl: 'docs/json'
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
