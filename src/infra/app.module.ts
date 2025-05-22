2d48470fdedcd9bf1178ced42b8615f4a7e343d1
Vinicius <viniciuspinheiro932@gmail.com>
2025-05-22 09:10:36 -0300

    feat(http): add env module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './http/http.module';
+import { EnvModule } from './env/env.module';
 
 @Module({
   imports: [
     DatabaseModule,
-    HttpModule
+    HttpModule,
+    EnvModule
   ],
   controllers: [],
   providers: [],
