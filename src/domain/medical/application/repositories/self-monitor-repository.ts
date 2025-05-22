f17d0fc6fc5c2ba615d2fbc162d48557eb5b63f7
Vinicius <viniciuspinheiro932@gmail.com>
2025-05-22 09:01:36 -0300

    fix(self-monitor): findByUserId method name changed to findByAccountId on the SelfMonitorRepository

--- src/domain/medical/application/repositories/self-monitor-repository.ts
+++ src/domain/medical/application/repositories/self-monitor-repository.ts
@@ -2,7 +2,7 @@ import { SelfMonitor } from "../../enterprise/entities/self-monitor";
 
 export abstract class SelfMonitorRepository {
   abstract findById(id: string): Promise<SelfMonitor | null>;
-  abstract findByUserId(userId: string): Promise<SelfMonitor | null>;
+  abstract findByAccountId(userId: string): Promise<SelfMonitor | null>;
   abstract findByCaregiverId(caregiverId: string): Promise<SelfMonitor[] | null>;
   abstract create(selfMonitor: SelfMonitor): Promise<void>;
   abstract save(selfMonitor: SelfMonitor): Promise<void>;
